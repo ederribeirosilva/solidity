35a4f68e3f033fc53359931de833946c292ecf77
Eder Ribeiro <eder_ribeiro@live.com>
2024-01-31 19:40:20 -0300

    Ajuste identacao vs code

--- aluguel.sol
+++ aluguel.sol
@@ -9,7 +9,8 @@ contract Aluguel {
     string public locatario;
     string public locador;
     uint256 private valor;
-    uint256 constant public numeroMaximoLegalDeAlugueisParaMulta = 3;
+    uint256 public constant numeroMaximoLegalDeAlugueisParaMulta = 3;
+
     bool[] public statusPagamento;
     /*
     0 - 01/2020 = true
@@ -20,67 +21,75 @@ contract Aluguel {
     address payable public contaLocador;
     address public owner;
 
-    constructor(    string memory _nomeLocador, 
-                    string memory _nomeLocatario, 
-                    address payable _contaLocador, 
-                    uint256 _valorDoAluguel)  payable {
+    constructor(
+        string memory _nomeLocador,
+        string memory _nomeLocatario,
+        address payable _contaLocador,
+        uint256 _valorDoAluguel
+    ) payable {
         locador = _nomeLocador;
         locatario = _nomeLocatario;
         valor = _valorDoAluguel;
         contaLocador = _contaLocador;
         owner = msg.sender;
     }
- 
+
     function valorAtualDoAluguel() public view returns (uint256) {
         return valor;
     }
- 
-    function simulaMulta( uint256 mesesRestantes, uint256 totalMesesContrato) public view returns(uint256 valorMulta) 
-    {
-        valorMulta = valor*numeroMaximoLegalDeAlugueisParaMulta;
-        valorMulta = valorMulta/totalMesesContrato;
-        valorMulta = valorMulta*mesesRestantes;
+
+    function simulaMulta(
+        uint256 mesesRestantes,
+        uint256 totalMesesContrato
+    ) public view returns (uint256 valorMulta) {
+        valorMulta = valor * numeroMaximoLegalDeAlugueisParaMulta;
+        valorMulta = valorMulta / totalMesesContrato;
+        valorMulta = valorMulta * mesesRestantes;
         return valorMulta;
-    } 
-        
-    function reajustaAluguel(uint256 percentualReajuste) public 
-    {
+    }
+
+    function reajustaAluguel(uint256 percentualReajuste) public {
         if (percentualReajuste > 20) {
             percentualReajuste = 20;
         }
         uint256 valorDoAcrescimo = 0;
-        valorDoAcrescimo = ((valor*percentualReajuste)/100);
+        valorDoAcrescimo = ((valor * percentualReajuste) / 100);
         valor = valor + valorDoAcrescimo;
     }
-    
-    function aditamentoValorAluguel(uint256 valorCerto) public   {
+
+    function aditamentoValorAluguel(uint256 valorCerto) public {
         valor = valorCerto;
     }
 
-    function aplicaMulta(uint256 mesesRestantes, uint256 percentual) public     {
-        require(mesesRestantes<30, "Periodo de contrato invalido");
-        for (uint numeroDeVoltas=0; numeroDeVoltas < mesesRestantes; numeroDeVoltas=numeroDeVoltas+2) {
-            valor = valor+((valor*percentual)/100);
+    function aplicaMulta(uint256 mesesRestantes, uint256 percentual) public {
+        require(mesesRestantes < 30, "Periodo de contrato invalido");
+        for (
+            uint numeroDeVoltas = 0;
+            numeroDeVoltas < mesesRestantes;
+            numeroDeVoltas = numeroDeVoltas + 2
+        ) {
+            valor = valor + ((valor * percentual) / 100);
         }
     }
-    
-    
+
     function receberPagamento() public payable {
-        require(msg.value>=valor, "Valor insuficiente");
+        require(msg.value >= valor, "Valor insuficiente");
         contaLocador.transfer(msg.value);
         statusPagamento.push(true);
     }
-    
+
     //msg.value = valor em wei enviado ao contrato
-    
-    function retornaTexto(uint256 _parametro) public view returns (string memory) {
+
+    function retornaTexto(
+        uint256 _parametro
+    ) public view returns (string memory) {
         if ((valor * _parametro) > 5000) {
             return "Muito caro";
         } else {
             return "Preco razoavel";
         }
     }
-    
+
     function quantosPagamentosJaForamFeitos() public view returns (uint256) {
         return statusPagamento.length;
     }
